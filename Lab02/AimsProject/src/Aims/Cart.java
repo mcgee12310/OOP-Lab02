05e2f7cee783a7519963d18c0c2af514179786e3
mcgee12310 <hieubinbom@gmail.com>
2024-10-27 16:47:39 +0700

    first commit

---
+++ Lab02/AimsProject/src/Aims/Cart.java
@@ -0,0 +1,40 @@
+package Aims;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc [MAX_NUMBERS_ORDERED];
+	public static int qtyOrdered = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added.");
+        } 
+        else {
+            System.out.println("The cart is full");
+        }
+    }
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc has been removed.");
+                break;
+            }
+        }
+    }
+
+    public float totalCost() {
+        float totalCost = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+        	totalCost += itemsOrdered[i].getCost();
+        }
+        return totalCost;
+    }
+}
