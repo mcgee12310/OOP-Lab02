40fc1ec3a874fc6c2afc117a7ae18541d9e8b8a7
mcgee12310 <hieubinbom@gmail.com>
2024-11-23 09:08:38 +0700

    passing parameter

--- Lab02/AimsProject/src/Aims/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/Aims/DigitalVideoDisc.java
@@ -47,5 +47,21 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
 	}		
 }
